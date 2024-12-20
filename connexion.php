15f86a29828cb0a62d065febed295a4e42efd750
Gueddes Imen <63718003+imentgds@users.noreply.github.com>
2024-12-20 17:54:24 +0100

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -4,36 +4,36 @@ include("Connect.php");
 $mdp = $_POST['password'];
 $email = $_POST['email'];
 
-$sql1 = "SELECT Email FROM membre_site WHERE Email = '$email'";
+$sql1 = "SELECT email FROM membre_site WHERE email = '$email'";
 $res1 = mysqli_query($com, $sql1);
 
 $existe = false;
 while ($row = mysqli_fetch_assoc($res1)) {
-    if ($email == $row['Email']) {
+    if ($email == $row['email']) {
         $existe = true;
     }
 }
 
 if ($existe == true) {
     $test = false;
-    $sql2 = "SELECT Password FROM compte WHERE Email = '$email'";
+    $sql2 = "SELECT mdp FROM membre_site WHERE email = '$email'";
     $res2 = mysqli_query($com, $sql2); // Correction de la variable $res2
 
     while ($row = mysqli_fetch_assoc($res2)) {
-        if ($mdp == $row['Password']) {
+        if ($mdp == $row['mdp']) {
             $test = true;
         }
     }
 
     if ($test == true) {
-       include("welcome.html");
+       include("welcom.html");
     } else {
         
-        include("er2.html");
+        include("erreur.html");
 
     }
 } else {
-    include("er3.html");
+    include("erreur.html");
    
 }
 ?>
--- inscri.php
+++ inscri.php
@@ -8,39 +8,16 @@ $email = $_POST['email'];
 $date=$_POST['dn'];
 
 
-$sql1 = "SELECT Email FROM membre_site WHERE Email = '$email'";
-$res1 = mysqli_query($com, $sql1);
-
-$existe = false;
-while ($row = mysqli_fetch_assoc($res1)) {
-    if ($email == $row['Email']) {
-        include("erreur.html");
-        $existe = true;
-    }
-}
-
-if (!$existe) {
-    // Insertion dans la table compte
-    $req2 = "INSERT INTO compte (Email, password) VALUES ('$email', '$mdp')";
-    $result_req2 = mysqli_query($com, $req2);
-
-    // Récupération de l'id_compte généré
-    $id_compte = mysqli_insert_id($com);
-
-    if ($id_compte) {
-        // Insertion dans la table membre_site avec l'id_compte généré
-        $sql = "INSERT INTO membre_site (nom, prenom, date_N, filiere, id_compte, Email) VALUES ('$nom', '$prenom', '$date', '$f', $id_compte, '$email')";
+        $sql = "INSERT INTO membre_site (nom , prenom , mdp , email , date ) VALUES ('$nom' , '$prenom' , '$mdp' , '$email' , '$date');";
         $result_sql = mysqli_query($com, $sql);
         
         if ($result_sql) {
-            include("liste.html");
+            include("welcom.html");
 
         } else {
             include("erreur.html");
         }
-    } else {
-        include("erreur.html");
-    }
-}
+  
+
 
 ?>
